ef407b15e895d521a6c2eb82df4ebe18a9cfab96
kim <kim.bechtold@webfactory-world.de>
2022-01-12 10:02:48 +0100

    Added a script to create the app client

--- matthews-app-backend/MatthewsApp.API/MatthewsApp.API/Resources/AddAppClient.sql
+++ matthews-app-backend/MatthewsApp.API/MatthewsApp.API/Resources/AddAppClient.sql
@@ -1,13 +1,14 @@
-﻿DECLARE @ClientName VARCHAR(max), @ClientDescription VARCHAR(max), @ClientId INT, @secretText varchar(max), @Hashbytes varbinary(128), @Secret VARCHAR(max), @CreationTime DateTime2, @RedirectUri varchar(max), @RedirectUriTesting varchar(max), @ApiName varchar(max);
+﻿DECLARE @ClientName VARCHAR(max), @ClientDescription VARCHAR(max), @ClientId INT, @secretText varchar(max), @Hashbytes varbinary(128), @Secret VARCHAR(max), @CreationTime DateTime2, @RedirectUri varchar(max), @PostLogoutRedirectUri varchar(max), @ApiName varchar(max), @MatthewsApiName varchar(max);
 SET @CreationTime = SYSUTCDATETIME();
 
 -- This values could be modified by user
-SET @ClientName = N'matthews.swagger'; 
-SET @ClientDescription = N'matthews.swagger Client';
-SET @secretText = '8f316032b4134fca974b44be7d3d816c';
-SET @RedirectUriTesting = N'https://localhost:44320/swagger/oauth2-redirect.html';
-SET @RedirectUri = N'https://matthewscremation.i4connected.cloud/matthews/swagger/oauth2-redirect.html';
-SET @ApiName = N'matthews.api'; 
+SET @ClientName = N'matthews.app'; 
+SET @ClientDescription = N'matthews.app Client';
+SET @secretText = 'b8f30abd541943ff92a1d36ca72ed25e';
+SET @RedirectUri = N'com.matthews.app://authorizationcallback';
+SET @PostLogoutRedirectUri = N'com.matthews.app://endsessioncallback';
+SET @MatthewsApiName = N'matthews.api'; 
+SET @ApiName = N'api'; 
 
 -- Creating a secret
 SET @Hashbytes = hashbytes('sha2_256', @secretText);
@@ -23,16 +24,12 @@ IF NOT EXISTS
       [BackChannelLogoutUri], [BackChannelLogoutSessionRequired], [AllowOfflineAccess], [IdentityTokenLifetime], [AllowedIdentityTokenSigningAlgorithms], [AccessTokenLifetime], [AuthorizationCodeLifetime],
       [ConsentLifetime], [AbsoluteRefreshTokenLifetime], [SlidingRefreshTokenLifetime], [RefreshTokenUsage], [UpdateAccessTokenClaimsOnRefresh], [RefreshTokenExpiration], [AccessTokenType],
          [EnableLocalLogin], [IncludeJwtId], [AlwaysSendClientClaims], [ClientClaimsPrefix], [PairWiseSubjectSalt], [Created], [Updated], [LastAccessed], [UserSsoLifetime], [UserCodeType], [DeviceCodeLifetime], [NonEditable]) 
-         VALUES (1, @ClientName, N'oidc', 1, @ClientDescription, NULL, NULL, NULL, 0, 1, 0, 0, 0, 0, 0, NULL, 1, NULL, 1, 1, 300, NULL, 3600, 300, NULL, 2592000, 1296000, 1, 0, 1, 1, 1, 1, 0, N'client_', NULL, @CreationTime, NULL, NULL, NULL, NULL, 300, 0)
+         VALUES (1, @ClientName, N'oidc', 1, @ClientDescription, NULL, NULL, NULL, 0, 1, 0, 1, 0, 0, 0, NULL, 1, NULL, 1, 1, 300, NULL, 3600, 300, NULL, 2592000, 1296000, 1, 0, 1, 1, 1, 1, 0, N'client_', NULL, @CreationTime, NULL, NULL, NULL, NULL, 300, 0)
 
       SET @ClientId = (SELECT Id FROM  [dbo].[Clients] WHERE ClientId = @ClientName)
 
       PRINT 'Creating a ClientGrantTypes, authorization_code';
       INSERT [dbo].[ClientGrantTypes] ([GrantType], [ClientId]) VALUES (N'authorization_code', @ClientId)
-
-      -- if authorization_code with Pkce is not posible may use this
-      -- PRINT 'Creating a ClientGrantTypes, implicit';
-      -- INSERT [dbo].[ClientGrantTypes] ([GrantType], [ClientId]) VALUES (N'implicit', @ClientId)
 
       PRINT 'Creating ClientScope profile';
       INSERT [dbo].[ClientScopes] ([Scope], [ClientId]) VALUES (N'profile', @ClientId)
@@ -40,17 +37,20 @@ IF NOT EXISTS
       INSERT [dbo].[ClientScopes] ([Scope], [ClientId]) VALUES (N'openid', @ClientId)
       PRINT 'Creating ClientScope email';
       INSERT [dbo].[ClientScopes] ([Scope], [ClientId]) VALUES (N'email', @ClientId)
-      PRINT 'Creating ClientScope for new api';
-      INSERT [dbo].[ClientScopes] ([Scope], [ClientId]) VALUES (@ApiName, @ClientId)      
+      PRINT 'Creating ClientScope for matthews api';
+      INSERT [dbo].[ClientScopes] ([Scope], [ClientId]) VALUES (@MatthewsApiName, @ClientId)   
+      PRINT 'Creating ClientScope for i4connected api';
+      INSERT [dbo].[ClientScopes] ([Scope], [ClientId]) VALUES (@ApiName, @ClientId)   
 
       PRINT 'Creating ClientSecret: ' + @secretText;
       INSERT [dbo].[ClientSecrets] ([ClientId], [Description], [Value], [Expiration], [Type], [Created]) VALUES (@ClientId, NULL, @Secret, NULL, N'SharedSecret', @CreationTime)
 	
-      PRINT 'Creating ClientRedirectUri for testing, needs to be removed in production: ' + @RedirectUriTesting;
-      INSERT [dbo].[ClientRedirectUris] ([RedirectUri], [ClientId]) VALUES (@RedirectUriTesting, @ClientId)
-
       PRINT 'Creating ClientRedirectUri: ' + @RedirectUri;
       INSERT [dbo].[ClientRedirectUris] ([RedirectUri], [ClientId]) VALUES (@RedirectUri, @ClientId)
+
+      PRINT 'Creating ClientPostLogoutRedirectUris: ' + @PostLogoutRedirectUri;
+      INSERT [dbo].ClientPostLogoutRedirectUris ([PostLogoutRedirectUri], [ClientId]) VALUES (@PostLogoutRedirectUri, @ClientId)
+
     END
 ELSE 
 	PRINT 'Client '+ @ClientName +' already exsists, please select an other name';
