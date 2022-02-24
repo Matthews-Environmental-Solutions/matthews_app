e1d39042f2a9498bb3a6bbb2c756f18e08614e9c
stojan.vukovic <stojan.vukovic@comdata.rs>
2022-02-24 13:52:51 +0100

    Matthews-App-Ionic: defined connection to the signalR hub in signalr service.

--- matthews-app/src/app/core/signal-r.service.ts
+++ matthews-app/src/app/core/signal-r.service.ts
@@ -1,73 +1,43 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Injectable } from '@angular/core';
 import { AuthService } from 'ionic-appauth';
 import { TokenResponse } from '@openid/appauth';
-declare var $: any;
+declare let $: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignalRService {
-  private connection: any;
-  private proxy: any;
 
   constructor(private authService: AuthService) {}
-  //public data: DataModelFromAPI[];
-
-  // private hubConnection: signalR.HubConnection;
 
   public initializeSignalRConnection(): void {
-    let signalRServerEndPoint = 'https://matthewscremation.i4connected.cloud/api/signalr';
-    this.connection = $.hubConnection(signalRServerEndPoint);
-    this.proxy = this.connection.createHubProxy('measurementHub');
+    const signalRServerEndPoint = 'https://matthewscremation.i4connected.cloud/api/signalr';
+    const connection = $.hubConnection(signalRServerEndPoint);
+    this.getAccessToken().then((token) => {
+      connection.qs = { access_token: token };
+      const proxy = connection.createHubProxy('measurementHub');
+      proxy.on('onMeasurement', this.onMessageReceived);
 
-    this.proxy.on('OnMeasurement', 'bbb709ea-4db7-4a9c-80d0-c45b10d137a5', (serverMessage) => this.onMessageReceived(serverMessage));
-    this.connection.logging = true;
-    this.connection.start().done((data: any) => {
-      console.log('Connected to Measurement Hub');
-      this.proxy.invoke('Subscribe', 'bbb709ea-4db7-4a9c-80d0-c45b10d137a5' );
-
-              //this.broadcastMessage();
-      }).catch((error: any) => {
-              console.log('Notification Hub error -> ' + error);
-      });
-    }
-
-  private onMessageReceived(serverMessage: string) {
-    console.log('New message received from Server: ' + serverMessage);
+      connection.start().done(() => {
+          console.log('Connected to Measurement Hub');
+          proxy.invoke('Subscribe', 'bbb709ea-4db7-4a9c-80d0-c45b10d137a5')
+            .done((measurement) => {
+                console.log(measurement);
+              });
+        }).fail((error) => {
+            console.log('Notification Hub error -> ' + error);
+          });
+    });
   }
 
- private broadcastMessage(): void {
-  this.proxy.invoke('NotificationService', 'text message')
-     .catch((error: any) => {
-         console.log('broadcastMessage error -> ' + error);
-      });
-    }
-
-  // public startConnection = () => {
-
-  //   this.hubConnection = new signalR.HubConnectionBuilder()
-  //   .withUrl('https://matthewscremation.i4connected.cloud/api/signalr', {
-  //     accessTokenFactory: () => this.getAccessToken()
-  //   })
-  //   .build();
-
-
-  //   this.hubConnection
-  //   .start()
-  //   .then(() => console.log("Connection started"))
-  //   .catch(err => console.log("Error while starting connection: " + err))
-  // }
-
-  // public addTemperatureDataListener = () => {
-  //   this.hubConnection.on('measurementHub', (data) => {
-  //    // this.data = data;
-  //     console.log(data);
-  //   })
-  // }
+  private onMessageReceived(measurement: any) {
+    console.log('New message received from Server: ' + measurement);
+  }
 
   public async getAccessToken() {
     const token: TokenResponse = await this.authService.getValidToken();
-    console.log(JSON.stringify(token));
     return token.accessToken;
   }
 
