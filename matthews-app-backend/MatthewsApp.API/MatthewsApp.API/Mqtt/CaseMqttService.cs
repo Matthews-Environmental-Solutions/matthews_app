b4f0d85065cb17c9a3b6dc634b1352054269b7f4
Branislav Kurbalija <branislav@comdata.rs>
2023-07-13 15:34:33 +0200

    Back-End: CaseMqttService has now better logic to establish connections and maintain them

--- matthews-app-backend/MatthewsApp.API/MatthewsApp.API/Mqtt/CaseMqttService.cs
+++ matthews-app-backend/MatthewsApp.API/MatthewsApp.API/Mqtt/CaseMqttService.cs
@@ -14,24 +14,22 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MatthewsApp.API.Mqtt;
 
 public class CaseMqttService : IHostedService
 {
-    private IMqttClient _mqttClient;
-
     // list keeps all instances alive, so they can be executed again.
     // To stop any IMqttClient just remove it from the list
-    private Dictionary<Guid, IMqttClient> _mqttClientsList = new Dictionary<Guid, IMqttClient>();
-
-    private Dictionary<Guid, Task> _mqttTaskList = new Dictionary<Guid, Task>();
+    private Dictionary<string, IMqttClient> _mqttClientsList = new Dictionary<string, IMqttClient>();
 
     private List<MqttConnectionSettingDto> _mqttConnectionSettings = new List<MqttConnectionSettingDto>();
-    
+
     private MqttFactory _mqttFactory;
     private readonly CaseHub _caseHub;
     private IConfiguration _configuration;
+    private ICollection<DeviceDto> _devices;
     private readonly ILogger<CaseMqttService> _logger;
     private CaseI4cHttpClientService _caseI4CHttpClientService;
 
@@ -53,7 +51,6 @@ public class CaseMqttService : IHostedService
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-
         // THIS CODE IS TO TEST SENDING MESSAGES VIA SIGNALR
         //var timer = new System.Timers.Timer(5000);
         //timer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
@@ -62,10 +59,10 @@ public class CaseMqttService : IHostedService
         try
         {
             // 1. GET ALL MATTHEWS DEVICES FROM ALL FACILITIES
-            ICollection<DeviceDto> devices = await _caseI4CHttpClientService.GetAllDevicesAsync();
+            _devices = await _caseI4CHttpClientService.GetAllDevicesAsync();
 
             // 2. then iterate devices, 
-            foreach (DeviceDto device in devices)
+            foreach (DeviceDto device in _devices)
             {
                 if (device.adapterId is null) continue;
 
@@ -95,11 +92,14 @@ public class CaseMqttService : IHostedService
         }
         catch (Exception ex)
         {
-
+            _logger.LogError($"Error on StartAsync: {ex.Message}");
             throw;
         }
 
-        // 2. MQTT
+        // 2. Continue to refresh list of devices each 15 minutes
+        GetDevicesAsync(); // must NOT BE await!!
+
+        // 3. MQTT
         ConnectToMqttBrokers();
     }
 
@@ -107,90 +107,156 @@ public class CaseMqttService : IHostedService
     {
         var mqttClientDisconnectOptions = _mqttFactory.CreateClientDisconnectOptionsBuilder().Build();
 
-        await _mqttClient.DisconnectAsync(mqttClientDisconnectOptions, CancellationToken.None);
+        foreach (KeyValuePair<string, IMqttClient> client in _mqttClientsList)
+        {
+            await client.Value.DisconnectAsync(mqttClientDisconnectOptions, CancellationToken.None);
+        }
 
         //return Task.CompletedTask;
-        //throw new System.NotImplementedException();
     }
 
-
+    /// <summary>
+    /// It will establich connection to all mqtt brokers that are used from all devices. More, it will subscribe to topic.
+    /// </summary>
     private void ConnectToMqttBrokers()
     {
-        foreach (MqttConnectionSettingDto setting in _mqttConnectionSettings)
-        {
-            var mqttFactory = new MqttFactory();
-            _mqttClient = mqttFactory.CreateMqttClient();
-
-            var options = new MqttClientOptionsBuilder()
-                .WithClientId(Guid.NewGuid().ToString())
-                .WithTcpServer(setting.Host, setting.Port)
-                .WithCredentials(setting.Username, setting.Password)
-                .WithTls()
-                .WithCleanSession()
-                .Build();
-
-            // DEFINE CALLBACKs to mqttClient events
-            _mqttClient.ApplicationMessageReceivedAsync += e =>
+        _ = Task.Run(
+            async () =>
             {
-                Debug.WriteLine("Received application message: " + e.ClientId + " " + e.ResponseReasonString);
-                return Task.CompletedTask;
-            };
-
-            //_mqttClient.ApplicationMessageReceivedAsync += delegate (MqttApplicationMessageReceivedEventArgs args)
-            //{
-            //    Debug.WriteLine("Received application message: " + args.ClientId);
-            //    return Task.CompletedTask;
-            //};
-
-            _mqttClient.ConnectedAsync += e =>
-            {
-                Debug.WriteLine("The MQTT client is connected.");
-                return Task.CompletedTask;
-            };
-
-            Task t = Task.Run(
-                async () =>
+                // User proper cancellation and no while(true).
+                while (true)
                 {
-                    // User proper cancellation and no while(true).
-                    while (true)
+                    try
                     {
-                        try
+                        foreach (MqttConnectionSettingDto setting in _mqttConnectionSettings)
                         {
+                            IMqttClient _mqttClient;
+
+                            // 1. check if connection to mqtt broker already exists. If yes just take it, if not create it and add it to Dictionary.
+                            if (_mqttClientsList.ContainsKey(setting.Host))
+                            {
+                                _mqttClient = _mqttClientsList.GetValueOrDefault(setting.Host);
+                            }
+                            else
+                            {
+                                _mqttClient = CreateMqttClient();
+                                _mqttClientsList.Add(setting.Host, _mqttClient);
+                            }
+
                             // This code will also do the very first connect! So no call to _ConnectAsync_ is required in the first place.
                             if (!await _mqttClient.TryPingAsync())
                             {
-                                await _mqttClient.ConnectAsync(options, CancellationToken.None);
-                                // Subscribe to topics when session is clean etc.
+                                MqttClientOptions options = new MqttClientOptionsBuilder()
+                                    .WithClientId(Guid.NewGuid().ToString())
+                                    .WithTcpServer(setting.Host, setting.Port)
+                                    .WithCredentials(setting.Username, setting.Password)
+                                        .WithTls()
+                                        .WithCleanSession()
+                                        .Build();
 
-                                //iterate by all devices
-                                var mqttSubscribeOptions = _mqttFactory.CreateSubscribeOptionsBuilder()
-                                    .WithTopicFilter(f =>
+                                await _mqttClient.ConnectAsync(options, CancellationToken.None);
+
+                                // collect all topics to subscribe for particular mqtt broker (host)
+                                List<string> topics = GetTopicsForMqttClient(setting.Host);
+
+                                // Subscribe to topics when session is clean etc.
+                                MqttClientSubscribeOptionsBuilder mqttSubscribeOptionsBuilder = _mqttFactory.CreateSubscribeOptionsBuilder();
+                                foreach (string topic in topics)
+                                {
+                                    mqttSubscribeOptionsBuilder.WithTopicFilter(f =>
                                     {
-                                        f.WithTopic(setting.Topic);
-                                    })
-                                    .Build();
+                                        f.WithTopic($"{topic}/CaseUpdate");
+                                    });
+                                }
+                                var mqttSubscribeOptions = mqttSubscribeOptionsBuilder.Build();
 
                                 await _mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
                                 Debug.WriteLine("MQTT client subscribed to topic.");
-
                             }
-                        }
-                        catch (Exception)
-                        {
-                            // Handle the exception properly (logging etc.).
-                            throw;
-                        }
-                        finally
-                        {
-                            // Check the connection state every 5 seconds and perform a reconnect if required.
-                            await Task.Delay(TimeSpan.FromSeconds(5));
+
                         }
                     }
-                });
+                    catch (Exception ex)
+                    {
+                        // Handle the exception properly (logging etc.).
+                        Debug.WriteLine("Error at MQTT connection establish: " + ex.Message);
+                    }
+                    finally
+                    {
+                        // Check the connection state every 10 seconds and perform a reconnect if required.
+                        await Task.Delay(TimeSpan.FromSeconds(10));
+                    }
+                }
+            });
 
-            _mqttTaskList.Add(setting.DeviceId, t);
-            _mqttClientsList.Add(setting.DeviceId, _mqttClient);
-        }
-        
+    }
+
+    /// <summary>
+    /// It will create new mqtt client with two events.
+    /// </summary>
+    /// <returns>IMqttClient</returns>
+    private IMqttClient CreateMqttClient()
+    {
+        var mqttFactory = new MqttFactory();
+        IMqttClient mqttClient = mqttFactory.CreateMqttClient();
+
+        // DEFINE CALLBACKs to mqttClient events
+        mqttClient.ApplicationMessageReceivedAsync += e =>
+        {
+            Debug.WriteLine("Received application message: " + e.ClientId + " " + e.ResponseReasonString);
+            return Task.CompletedTask;
+        };
+
+        //_mqttClient.ApplicationMessageReceivedAsync += delegate (MqttApplicationMessageReceivedEventArgs args)
+        //{
+        //    Debug.WriteLine("Received application message: " + args.ClientId);
+        //    return Task.CompletedTask;
+        //};
+
+        mqttClient.ConnectedAsync += e =>
+        {
+            Debug.WriteLine("The MQTT client is connected.");
+            return Task.CompletedTask;
+        };
+
+        return mqttClient;
+    }
+
+    /// <summary>
+    /// It is a Task which will refresh the list of devices each N minutes.
+    /// The N minutes is taken from configuration. When you call it, do not use "await".
+    /// </summary>
+    /// <returns>void</returns>
+    private async Task GetDevicesAsync()
+    {
+        _ = Task.Run(
+            async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        _devices = await _caseI4CHttpClientService.GetAllDevicesAsync();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                    finally
+                    {
+                        await Task.Delay(TimeSpan.FromMinutes(Int32.Parse(_configuration["deviceListRefrechIntervalInMinutes"])));
+                    } 
+                }
+            });
+    }
+
+    /// <summary>
+    /// It will find all topics related to particular mqtt Broker (host).
+    /// </summary>
+    /// <param name="HostUrl"></param>
+    /// <returns>List of topics</returns>
+    private List<string> GetTopicsForMqttClient(string HostUrl)
+    {
+        return _mqttConnectionSettings.Where(s => s.Host == HostUrl).Select(s => s.Topic).ToList();
     }
 }
