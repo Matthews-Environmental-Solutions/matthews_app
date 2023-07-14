e3bde8d9d4495ecbd83c8883dda22364f8ec173c
Nikola <nikola.hajder@comdata.com>
2023-07-14 12:47:31 +0200

    Mob-App schedule tab formated

---
+++ matthews-app/src/app/case/selection-option.ts
@@ -0,0 +1,19 @@
+export class ContainerSize {
+    id!: number;
+    name!: string;
+}
+
+export class ContainerType {
+    id!: number;
+    name!: string;
+}
+
+export class GenderType {
+    id!: number;
+    name!: string;
+}
+
+export class CaseStatus {
+    id!: number;
+    name!: string;
+}
