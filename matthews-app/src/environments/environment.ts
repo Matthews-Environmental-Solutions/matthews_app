2269b44d924a61088cc9f68660e2f5c7709c0870
Nikola <nikola.hajder@comdata.com>
2024-11-13 15:54:06 +0100

    Mob-App: Emulator work

--- matthews-app/src/environments/environment.ts
+++ matthews-app/src/environments/environment.ts
@@ -3,6 +3,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// web
 export const environment = {
   production: false,
   apiUrl: 'https://develop.comdata.rs/MatthewsApp.API',
@@ -18,34 +19,18 @@ export const environment = {
   }
 };
 
-//matthews web
+// mob
 // export const environment = {
 //   production: false,
-//   apiUrl: 'https://matthewscremation.i4connected.cloud/MatthewsApp.API',
-//   i4connectedApiUrl: 'https://matthewscremation.i4connected.cloud/api',
-//   auth_config: {
-//     client_id: 'matthews.web',
-//     client_secret: '0be0470165fa49ca9631a2babc0a73d4',
-//     server_host: 'https://matthewscremation.i4connected.cloud/identity',
-//     redirect_url: 'https://matthewscremation.i4connected.cloud/MatthewsApp.Mobile/authorizationcallback',
-//     end_session_redirect_url: 'https://matthewscremation.i4connected.cloud/MatthewsApp.Mobile/authorizationcallback',
-//     scopes: 'profile openid email offline_access matthews.api api',
-//     pkce: true
-//   }
-// };
-
-// matthews mob
-// export const environment = {
-//   production: false,
-//     apiUrl: 'https://matthewscremation.i4connected.cloud/MatthewsApp.API',
-//     i4connectedApiUrl: 'https://matthewscremation.i4connected.cloud/api',
+//   apiUrl: 'https://develop.comdata.rs/MatthewsApp.API',
+//   i4connectedApiUrl: 'https://matthewsenvironmental.i4connected.cloud/api',
 //   auth_config: {
 //     client_id: 'matthews.app',
 //     client_secret: 'b8f30abd541943ff92a1d36ca72ed25e',
-//     server_host: 'https://matthewscremation.i4connected.cloud/identity',
+//     server_host: 'https://matthewsenvironmental.i4connected.cloud/identity',
 //     redirect_url: 'com.matthews.app://authorizationcallback',
 //     end_session_redirect_url: 'com.matthews.app://endsessioncallback',
-//     scopes: 'profile openid email api matthews.api',
+//     scopes: 'profile openid email offline_access matthews.api api',
 //     pkce: true
 //   }
 // };
