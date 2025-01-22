c2bddd094ccff839c063f17ccdfc2e80897589eb
Nikola <nikola.hajder@comdata.com>
2025-01-22 14:06:02 +0100

    Mob-App: removed status 4

--- matthews-app/src/app/cremation-process/cremation-process.page.ts
+++ matthews-app/src/app/cremation-process/cremation-process.page.ts
@@ -70,9 +70,8 @@ export class CremationProcessPage implements OnInit, OnDestroy {
         }
         if (
           signal.name === 'MACHINE_STATUS' &&
-          (parseInt(signal.value) >= 40 &&
-            parseInt(signal.value) < 50) ||
-          parseInt(signal.value) == 4
+          parseInt(signal.value) >= 40 &&
+            parseInt(signal.value) < 50
         ) {
           this.move(1);
         } else if (
